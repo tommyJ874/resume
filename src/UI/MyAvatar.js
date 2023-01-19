79e0fecff84b12c4a5be2c9f407b48f209adc1ef
tommyJ874 <tommyJ.dev@gmail.com>
2023-01-19 17:23:55 +0900

    Fix 모바일 대응

--- src/UI/MyAvatar.js
+++ src/UI/MyAvatar.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const MyAvatar = (props) => {
   return (
-    <div className='h-[12.5rem] w-[12.5rem] min-w-max md:mr-10 '>
-      <div className='h-full w-full rounded-full bg-gray-50'>
-        <img
-          className='h-full w-full rounded-full'
-          src={props.src}
-          alt='portrait'
-        />
+    <div className='flex justify-center'>
+      <div className='h-[12.5rem] w-[12.5rem] min-w-max md:mr-10 '>
+        <div className='h-full w-full rounded-full bg-gray-50'>
+          <img
+            className='h-full w-full rounded-full'
+            src={props.src}
+            alt='portrait'
+          />
+        </div>
       </div>
     </div>
   );
